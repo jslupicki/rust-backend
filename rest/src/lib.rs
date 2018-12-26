7d46fb66f732c6e97a8ff02760b5bf1503a8272d
jslupicki <jan@slupicki.com>
2018-12-26 12:13:41 +0100

    First REST endpoint (/users) with test HTTP request - now just static response

--- rest/src/lib.rs
+++ rest/src/lib.rs
@@ -3,20 +3,45 @@ extern crate dao;
 #[macro_use]
 extern crate log;
 extern crate log4rs;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
 
-use actix_web::{App, HttpRequest, server};
+use actix_web::{App, Error, HttpRequest, HttpResponse, Responder, server};
+use actix_web::http::Method;
 
 use dao::test;
+
+#[derive(Serialize, Deserialize)]
+struct UserDTO {
+    username: String
+}
 
 fn index(_req: &HttpRequest) -> &'static str {
     info!("Got request!");
     "Hello world!"
 }
 
+fn get_users(_req: &HttpRequest) -> Result<HttpResponse, Error> {
+    let users = vec![
+        UserDTO { username: "Test1".to_string() },
+        UserDTO { username: "Test2".to_string() },
+        UserDTO { username: "Test3".to_string() },
+    ];
+    let body = serde_json::to_string(&users)?;
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(body))
+}
+
 pub fn start() {
     info!("Start application");
 
-    server::new(|| App::new().resource("/", |r| r.f(index)))
+    server::new(|| App::new()
+        .resource("/", |r| r.f(index))
+        .resource("/users", |r| r.method(Method::GET).f(get_users))
+    )
         .bind("127.0.0.1:8088")
         .unwrap()
         .run();
