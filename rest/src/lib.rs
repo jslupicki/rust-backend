55106692ecb045a4afdc4aa1e867c3402e018f22
jslupicki <jan@slupicki.com>
2018-12-29 14:54:34 +0100

    Start working on middleware

--- rest/src/lib.rs
+++ rest/src/lib.rs
@@ -10,11 +10,25 @@ extern crate serde_json;
 
 use actix_web::{App, Error, HttpRequest, HttpResponse, Responder, server};
 use actix_web::http::Method;
+use actix_web::middleware::{Middleware, Response, Started};
 
 #[derive(Serialize, Deserialize)]
 struct UserDTO {
     username: String
 }
+
+struct Headers;
+
+impl<S> Middleware<S> for Headers {
+
+    /// Method is called when request is ready. It may return
+    /// future, which should resolve before next middleware get called.
+    fn start(&self, req: &HttpRequest<S>) -> Result<Started, Error> {
+        info!("GOT REQUEST for {}", req.path());
+        Ok(Started::Done)
+    }
+}
+
 
 fn index(_req: &HttpRequest) -> &'static str {
     info!("Got request!");
@@ -44,6 +58,7 @@ pub fn start() {
     info!("Start application");
 
     server::new(|| App::new()
+        .middleware(Headers)
         .resource("/", |r| r.f(index))
         .resource("/users", |r| r.method(Method::GET).f(get_users))
     )
