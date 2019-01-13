60a28917bd0c34b58913025214292e4e2eb4f95c
jslupicki <jan@slupicki.com>
2019-01-13 17:01:48 +0100

    Set cookie test

--- rest/src/lib.rs
+++ rest/src/lib.rs
@@ -1,4 +1,5 @@
 extern crate actix_web;
+extern crate cookie;
 extern crate dao;
 #[macro_use]
 extern crate log;
@@ -13,6 +14,7 @@ use std::ops::Deref;
 use actix_web::{App, Error, HttpRequest, HttpResponse, Responder, server};
 use actix_web::http::Method;
 use actix_web::middleware::{Middleware, Response, Started};
+use cookie::Cookie;
 
 #[derive(Serialize, Deserialize)]
 struct UserDTO {
@@ -22,30 +24,33 @@ struct UserDTO {
 struct Headers;
 
 impl<S> Middleware<S> for Headers {
-
     /// Method is called when request is ready. It may return
     /// future, which should resolve before next middleware get called.
     fn start(&self, req: &HttpRequest<S>) -> Result<Started, Error> {
         info!("GOT REQUEST for {}", req.path());
         let a = req.cookie("a");
         info!("Cookie a={:?}", a);
-        let cookies = req.cookies().unwrap();
-        for c in cookies.deref() {
+        for c in req.cookies().unwrap().deref() {
             info!("Cookie: {:?}", c);
         }
         Ok(Started::Done)
     }
 }
 
-fn index(_req: &HttpRequest) -> &'static str {
+fn index(_req: &HttpRequest) -> Result<HttpResponse, Error> {
     info!("Got request!");
-    "Hello world!"
+    Ok(
+        HttpResponse::Ok()
+            .cookie(Cookie::new("key", "value"))
+            .content_type("text/html")
+            .body("Hello world")
+    )
 }
 
 fn get_users(_req: &HttpRequest) -> Result<HttpResponse, Error> {
     let users: Vec<UserDTO> = dao::get_users()
         .into_iter()
-        .map(|u| UserDTO { username: u.username})
+        .map(|u| UserDTO { username: u.username })
         .collect()
         ;
     let body = serde_json::to_string(&users)?;
