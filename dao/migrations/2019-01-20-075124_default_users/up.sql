4392998e3618b1ec6d586ac7fa7403ad02778293
jslupicki <jan@slupicki.com>
2019-05-01 19:13:48 +0200

    Fix SQL migrations - SQLite BOOLEAN have values 1/0

--- dao/migrations/2019-01-20-075124_default_users/up.sql
+++ dao/migrations/2019-01-20-075124_default_users/up.sql
@@ -1,3 +1,5 @@
 -- Default users user/user and admin/admin - password as SHA3 256
-insert into main.users(username, password, is_admin) values ('user', '8ac76453d769d4fd14b3f41ad4933f9bd64321972cd002de9b847e117435b08b', 'f');
-insert into main.users(username, password, is_admin) values ('admin', 'fb001dfcffd1c899f3297871406242f097aecf1a5342ccf3ebcd116146188e4b', 't');
+insert into main.users(username, password, is_admin)
+values ('user', '8ac76453d769d4fd14b3f41ad4933f9bd64321972cd002de9b847e117435b08b', 0);
+insert into main.users(username, password, is_admin)
+values ('admin', 'fb001dfcffd1c899f3297871406242f097aecf1a5342ccf3ebcd116146188e4b', 1);
