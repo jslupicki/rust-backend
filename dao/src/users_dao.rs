03f336a407730296f1b20894af57a6d8e5e46d04
jslupicki <jan@slupicki.com>
2019-05-11 08:02:15 +0200

    Switch SQLite in tests to in-memory DB - no need to single threaded tests

--- dao/src/users_dao.rs
+++ dao/src/users_dao.rs
@@ -69,26 +69,19 @@ pub fn initialize_db(conn: &SqliteConnection) -> Result<(), RunMigrationsError> 
 mod tests {
     use std::fs::remove_file;
     use std::io::stdout;
-    use std::sync::Arc;
 
     use diesel;
     use diesel::result::DatabaseErrorKind::UniqueViolation;
     use diesel::result::Error::DatabaseError;
     use log4rs;
-    use monitor::Monitor;
 
     use super::*;
 
-    static TEST_DB_NAME: &str = "test_db.sqlite3";
-
-    lazy_static! {
-        static ref MON: Arc<Monitor<bool>> = Arc::new(Monitor::new(false));
-    }
+    static TEST_DB_NAME: &str = ":memory:";
 
     embed_migrations!("./migrations");
 
     fn initialize_db() -> SqliteConnection {
-        let _ = remove_file(TEST_DB_NAME); // Ignore error when there is no TEST_DB_NAME file
         let conn = SqliteConnection::establish(TEST_DB_NAME)
             .expect(&format!("Error connecting to {}", TEST_DB_NAME));
         embedded_migrations::run_with_output(&conn, &mut stdout()).unwrap();
@@ -106,57 +99,55 @@ mod tests {
 
     #[test]
     fn crud_operations_on_user() {
-        MON.with_lock(|_| {
-            // Initialize
-            let _ = log4rs::init_file("log4rs.yml", Default::default());
-            let conn = &initialize_db();
+        // Initialize
+        let _ = log4rs::init_file("log4rs.yml", Default::default());
+        let conn = &initialize_db();
 
-            let initially_user_count = user_count(conn);
+        let initially_user_count = user_count(conn);
 
-            let test_user = "test_admin";
-            let test_updated_user = "test_updated_admin";
-            let test_pass = "test_admin_pass";
+        let test_user = "test_admin";
+        let test_updated_user = "test_updated_admin";
+        let test_pass = "test_admin_pass";
 
-            // Insert new_user
-            let new_user = NewUser {
-                username: test_user.to_string(),
-                password: test_pass.to_string(),
-                is_admin: true,
-            };
-            let rows_inserted = insert_into(users).values(&new_user).execute(conn);
-            assert_eq!(Ok(1), rows_inserted);
-            assert_user_count(initially_user_count + 1, conn);
+        // Insert new_user
+        let new_user = NewUser {
+            username: test_user.to_string(),
+            password: test_pass.to_string(),
+            is_admin: true,
+        };
+        let rows_inserted = insert_into(users).values(&new_user).execute(conn);
+        assert_eq!(Ok(1), rows_inserted);
+        assert_user_count(initially_user_count + 1, conn);
 
-            // Read user
-            let users_in_db = users
-                .filter(username.eq(&new_user.username))
-                .limit(2)
-                .load::<User>(conn)
-                .expect("Error loading users");
-            assert_eq!(1, users_in_db.len());
-            let user = &users_in_db[0];
-            assert_eq!(&new_user.username, &user.username);
-            assert_eq!(&new_user.password, &user.password);
-            assert_eq!(&new_user.is_admin, &user.is_admin);
+        // Read user
+        let users_in_db = users
+            .filter(username.eq(&new_user.username))
+            .limit(2)
+            .load::<User>(conn)
+            .expect("Error loading users");
+        assert_eq!(1, users_in_db.len());
+        let user = &users_in_db[0];
+        assert_eq!(&new_user.username, &user.username);
+        assert_eq!(&new_user.password, &user.password);
+        assert_eq!(&new_user.is_admin, &user.is_admin);
 
-            // Update username to "new_admin"
-            let rows_updated = diesel::update(users.filter(id.eq(user.id)))
-                .set(username.eq(test_updated_user.to_string()))
-                .execute(conn);
-            assert_eq!(Ok(1), rows_updated);
+        // Update username to "new_admin"
+        let rows_updated = diesel::update(users.filter(id.eq(user.id)))
+            .set(username.eq(test_updated_user.to_string()))
+            .execute(conn);
+        assert_eq!(Ok(1), rows_updated);
 
-            // Read updated_user and check if have changed username
-            let updated_user = users.filter(id.eq(user.id)).first::<User>(conn).unwrap();
-            assert_eq!(&test_updated_user.to_string(), &updated_user.username);
-            assert_eq!(&new_user.password, &updated_user.password);
-            assert_eq!(&new_user.is_admin, &updated_user.is_admin);
+        // Read updated_user and check if have changed username
+        let updated_user = users.filter(id.eq(user.id)).first::<User>(conn).unwrap();
+        assert_eq!(&test_updated_user.to_string(), &updated_user.username);
+        assert_eq!(&new_user.password, &updated_user.password);
+        assert_eq!(&new_user.is_admin, &updated_user.is_admin);
 
-            // Delete user from DB and DB should be in initial state
-            diesel::delete(users.filter(id.eq(user.id)))
-                .execute(conn)
-                .unwrap();
-            assert_user_count(initially_user_count, conn);
-        })
+        // Delete user from DB and DB should be in initial state
+        diesel::delete(users.filter(id.eq(user.id)))
+            .execute(conn)
+            .unwrap();
+        assert_user_count(initially_user_count, conn);
     }
 
     #[test]
@@ -173,59 +164,54 @@ mod tests {
 
     #[test]
     fn check_get_users() {
-        MON.with_lock(|_| {
-            // Initialize
-            let _ = log4rs::init_file("log4rs.yml", Default::default());
-            let conn = &initialize_db();
-            let initially_user_count = user_count(conn);
-            let all_users = get_users(conn);
-            assert_eq!(all_users.len() as i64, initially_user_count)
-        })
+        // Initialize
+        let _ = log4rs::init_file("log4rs.yml", Default::default());
+        let conn = &initialize_db();
+        let initially_user_count = user_count(conn);
+        let all_users = get_users(conn);
+        assert_eq!(all_users.len() as i64, initially_user_count)
     }
 
     #[test]
     fn check_validate_user() {
-        MON.with_lock(|_| {
-            // Initialize
-            let _ = log4rs::init_file("log4rs.yml", Default::default());
-            let conn = &initialize_db();
-            assert_eq!(
-                true,
-                validate_user(
-                    &"admin".to_string(),
-                    &"fb001dfcffd1c899f3297871406242f097aecf1a5342ccf3ebcd116146188e4b".to_string(),
-                    conn,
-                )
-            );
-            assert_eq!(
-                false,
-                validate_user(
-                    &"wrong".to_string(),
-                    &"fb001dfcffd1c899f3297871406242f097aecf1a5342ccf3ebcd116146188e4b".to_string(),
-                    conn,
-                )
-            );
-            assert_eq!(
-                false,
-                validate_user(&"admin".to_string(), &"wrong".to_string(), conn)
-            );
-        })
+        // Initialize
+        let _ = log4rs::init_file("log4rs.yml", Default::default());
+        let conn = &initialize_db();
+        assert_eq!(
+            true,
+            validate_user(
+                &"admin".to_string(),
+                &"fb001dfcffd1c899f3297871406242f097aecf1a5342ccf3ebcd116146188e4b".to_string(),
+                conn,
+            )
+        );
+        assert_eq!(
+            false,
+            validate_user(
+                &"wrong".to_string(),
+                &"fb001dfcffd1c899f3297871406242f097aecf1a5342ccf3ebcd116146188e4b".to_string(),
+                conn,
+            )
+        );
+        assert_eq!(
+            false,
+            validate_user(&"admin".to_string(), &"wrong".to_string(), conn)
+        );
     }
 
     #[test]
     fn should_prevent_creating_users_with_the_same_username() {
-        MON.with_lock(|_| {
-            // Initialize
-            let _ = log4rs::init_file("log4rs.yml", Default::default());
-            let conn = &initialize_db();
-            // Insert new_user
-            let new_user = NewUser {
-                username: "admin".to_string(),
-                password: "not_important".to_string(),
-                is_admin: true,
-            };
-            let rows_inserted = insert_into(users).values(&new_user).execute(conn);
-            match rows_inserted {
+        // Initialize
+        let _ = log4rs::init_file("log4rs.yml", Default::default());
+        let conn = &initialize_db();
+        // Insert new_user
+        let new_user = NewUser {
+            username: "admin".to_string(),
+            password: "not_important".to_string(),
+            is_admin: true,
+        };
+        let rows_inserted = insert_into(users).values(&new_user).execute(conn);
+        match rows_inserted {
                 Err(DatabaseError(UniqueViolation, msg)) => {
                     assert_eq!(msg.message(), "UNIQUE constraint failed: users.username")
                 }
@@ -234,57 +220,50 @@ mod tests {
                     format!("Should report: UNIQUE constraint failed: users.username and instead I got {:?}", rows_inserted)
                 ),
             }
-        })
     }
 
     #[test]
     fn check_get_user() {
-        MON.with_lock(|_| {
-            // Initialize
-            let _ = log4rs::init_file("log4rs.yml", Default::default());
-            let conn = &initialize_db();
-            let user_in_db = get_user(2, conn).unwrap();
-            assert_eq!("admin", user_in_db.username);
-            assert_eq!(true, user_in_db.is_admin);
-        })
+        // Initialize
+        let _ = log4rs::init_file("log4rs.yml", Default::default());
+        let conn = &initialize_db();
+        let user_in_db = get_user(2, conn).unwrap();
+        assert_eq!("admin", user_in_db.username);
+        assert_eq!(true, user_in_db.is_admin);
     }
 
     #[test]
     fn check_update_user() {
-        MON.with_lock(|_| {
-            // Initialize
-            let _ = log4rs::init_file("log4rs.yml", Default::default());
-            let conn = &initialize_db();
-            let mut admin_in_db = get_user(2, conn).unwrap();
-            admin_in_db.password = "new_password".to_string();
-            let updated_rows = update_user(&admin_in_db, conn);
-            assert!(updated_rows.is_ok());
-            let admin_in_db = get_user(2, conn).unwrap();
-            assert_eq!("new_password".to_string(), admin_in_db.password);
-            let updated_user = updated_rows.unwrap();
-            assert_eq!(2, updated_user.id);
-            assert_eq!("admin".to_string(), updated_user.username);
-            assert_eq!("new_password".to_string(), updated_user.password);
-            assert_eq!(true, updated_user.is_admin);
-        })
+        // Initialize
+        let _ = log4rs::init_file("log4rs.yml", Default::default());
+        let conn = &initialize_db();
+        let mut admin_in_db = get_user(2, conn).unwrap();
+        admin_in_db.password = "new_password".to_string();
+        let updated_rows = update_user(&admin_in_db, conn);
+        assert!(updated_rows.is_ok());
+        let admin_in_db = get_user(2, conn).unwrap();
+        assert_eq!("new_password".to_string(), admin_in_db.password);
+        let updated_user = updated_rows.unwrap();
+        assert_eq!(2, updated_user.id);
+        assert_eq!("admin".to_string(), updated_user.username);
+        assert_eq!("new_password".to_string(), updated_user.password);
+        assert_eq!(true, updated_user.is_admin);
     }
 
     #[test]
     fn create_user_should_return_created_user() {
-        MON.with_lock(|_| {
-            // Initialize
-            let _ = log4rs::init_file("log4rs.yml", Default::default());
-            let conn = &initialize_db();
-            let new_user = NewUser {
-                username: "new_username".to_string(),
-                password: "new_password".to_string(),
-                is_admin: false,
-            };
-            let created_user = create_user(&new_user, conn).unwrap();
-            assert_eq!(3, created_user.id);
-            assert_eq!(new_user.username, created_user.username);
-            assert_eq!(new_user.password, created_user.password);
-            assert_eq!(new_user.is_admin, created_user.is_admin);
-        })
+        // Initialize
+        let _ = log4rs::init_file("log4rs.yml", Default::default());
+        let conn = &initialize_db();
+        let new_user = NewUser {
+            username: "new_username".to_string(),
+            password: "new_password".to_string(),
+            is_admin: false,
+        };
+        let created_user = create_user(&new_user, conn).unwrap();
+        assert_eq!(3, created_user.id);
+        assert_eq!(new_user.username, created_user.username);
+        assert_eq!(new_user.password, created_user.password);
+        assert_eq!(new_user.is_admin, created_user.is_admin);
     }
 }
